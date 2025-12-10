a51f08f3cefaef09f0662eeb472cf63dea24536c
Sneha Sachan <snehasachan604@gmail.com>
2025-12-10 14:33:08 +0530

    Add a files I used in my website.

---
+++ script.js
@@ -0,0 +1,11 @@
+burger = document.querySelector(".burger ");
+navList = document.querySelector(".nav-list");
+navbar = document.querySelector(".navbar")
+
+burger.addEventListener("click", () => {
+  navList.classList.toggle("v-class");
+  navbar.classList.toggle("v-class");
+  navbar.classList.toggle("h-nav");
+  navList.classList.toggle("h-nav");
+
+});
